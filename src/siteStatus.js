faf54edd3ebe0e16522a6472d53222b3bb471937
saveTochi <nwangumat@hotmail.com>
2019-11-27 14:56:13 +0100

    Display temporary page completion status

---
+++ src/siteStatus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import underConstruction from './under_construction.svg';
+import './siteStatus.css';
+
+export default () => {
+  return (
+    <div className="text-box">
+      <img src={underConstruction} className="logo" alt="under construction sign" />
+      <h3 className="text"><span role="img" aria-label="sad face emoji">&#128554;</span> HireJuniors is currently under construction, please come back soon!</h3>
+    </div>
+  )
+}
