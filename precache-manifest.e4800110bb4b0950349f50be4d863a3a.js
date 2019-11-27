9b5bd81b3c0a5297532a8ce031161246e84c1846
saveTochi <nwangumat@hotmail.com>
2019-11-27 15:08:27 +0100

    Updates

---
+++ precache-manifest.e4800110bb4b0950349f50be4d863a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f940004a2d18bb214427f15f257f21",
+    "url": "/hirejuniors/index.html"
+  },
+  {
+    "revision": "1e42971df55ade1b3c46",
+    "url": "/hirejuniors/static/css/main.b6a293ee.chunk.css"
+  },
+  {
+    "revision": "b64c08a83f360ececd0b",
+    "url": "/hirejuniors/static/js/2.d3a95dea.chunk.js"
+  },
+  {
+    "revision": "1e42971df55ade1b3c46",
+    "url": "/hirejuniors/static/js/main.01c08570.chunk.js"
+  },
+  {
+    "revision": "0ee5ee30d90bccc39005",
+    "url": "/hirejuniors/static/js/runtime-main.8fe534be.js"
+  },
+  {
+    "revision": "fa940b32581a81c0bf4d32acc4a844ab",
+    "url": "/hirejuniors/static/media/under_construction.fa940b32.svg"
+  }
+]);
