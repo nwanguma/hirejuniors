06552da3b83da00cd0eed932b89e1ef39c98de79
Tochukwu Nwanguma <Tochukwu@cico.ng>
2020-06-22 22:53:32 +0100

    Add alt texts to sidebar logo and icons for screen readers

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -20,55 +20,67 @@ export const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.logo}>
-        <img src={logoAlt} />
+        <img src={logoAlt} alt="hirejuniors logo in alternative colour" />
         <span>ireJuniors</span>
       </div>
       <div className={styles.navigation}>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/overview">
-            <img src={stats} />
+            <img src={stats} alt="stats icon represents overview" />
             <span>Overview</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/profile">
-            <img src={profile} />
+            <img src={profile} alt="user silhoutte icon represents profile" />
             <span>Profile</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/verification">
-            <img src={verification} />
+            <img
+              src={verification}
+              alt="checklist icon represents verification"
+            />
             <span>Verification</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/manage-applications">
-            <img src={applications} />
+            <img
+              src={applications}
+              alt="applications icon represents applications"
+            />
             <span>Applications</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/jobs">
-            <img src={jobs} />
+            <img src={jobs} alt="classified ads icon represents list of jobs" />
             <span>Jobs</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/articles">
-            <img src={articles} />
+            <img
+              src={articles}
+              alt="newspaper wireframe icon represents articles"
+            />
             <span>Articles</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/portfolio">
-            <img src={portfolio} />
+            <img src={portfolio} alt="work tray icon represents portfolio" />
             <span>Portfolio</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/resources">
-            <img src={resources} />
+            <img
+              src={resources}
+              alt="cross icon represents resources available"
+            />
             <span>Resources</span>
           </NavLink>
         </div>
@@ -76,13 +88,13 @@ export const Sidebar = () => {
       <div className={styles.settings}>
         <div className={styles.nav_item}>
           <NavLink to="/dashboard/settings">
-            <img src={settings} />
+            <img src={settings} alt="gear icon represents settings" />
             <span>Settings</span>
           </NavLink>
         </div>
         <div className={styles.nav_item}>
           <NavLink to="/logout">
-            <img src={logout} />
+            <img src={logout} alt="exit icon represents logout" />
             <span>Logout</span>
           </NavLink>
         </div>
