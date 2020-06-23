c8dc5b5d2a66c9eabc12b50f21da84fad0f1a6e7
Tochukwu Nwanguma <Tochukwu@cico.ng>
2020-06-23 10:46:53 +0100

    Create dashboard overview grid sections

--- client/src/pages/dashboard/Overview.js
+++ client/src/pages/dashboard/Overview.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import styles from "./Overview.module.scss";
+
 export const Overview = (props) => {
-  return <div>This is the overview page</div>;
+  return (
+    <div className={styles.overview}>
+      <div className={styles.page_views}>page view</div>
+      <div className={styles.application_count}>application</div>
+      <div className={styles.ranking}>ranking</div>
+      <div className={styles.portfolio_strength}>portfolio</div>
+      <div className={styles.activity}>activity</div>
+      <div className={styles.recommended}>recommended</div>
+      <div className={styles.job_snippets}>Jobs</div>
+      <div className={styles.ads}>Ads</div>
+    </div>
+  );
 };
 
 export default Overview;
